f72e661afc959975d90d8d2030fddff81203e1a5
MargaretLai <MargaretLUI01@gmail.com>
2024-07-27 11:01:00 -0400

    Added form to get user input in review component

--- appone/forms.py
+++ appone/forms.py
@@ -1,7 +1,12 @@
 from django import forms
-from .models import Prep
+from .models import Prep, Review
 
 class PrepForm(forms.ModelForm):
     class Meta:
         model = Prep
+        fields = ["subject"]
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
         fields = ["subject"]
--- appone/views.py
+++ appone/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render, redirect, HttpResponse
-from .forms import PrepForm
+from .forms import PrepForm, ReviewForm
 
 # Create your views here.
 def home(request):
     return render(request, "home.html")
 
 def review(request):
-    return render(request, "review.html")
+    if request.method == "POST":
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            saved_form = form.save()
+            subject = saved_form.subject
+            print(subject)
+            return redirect("review") # Return to the same page rn
+    else:
+        form = ReviewForm()
+
+    return render(request, "review.html", {"form" : form})
 
 def prep(request):
     if request.method == "POST":
@@ -14,9 +24,9 @@ def prep(request):
         if form.is_valid():
             saved_form = form.save()
             subject = saved_form.subject
-            print(subject)
             return redirect("prep") # Return to the same page rn
     else:
         form = PrepForm()
         
     return render(request, "prep.html", {"form" : form})
+
