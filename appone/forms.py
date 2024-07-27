5cc23e810716b5a8b966d8bae3f25d63f69816e4
MargaretLai <MargaretLUI01@gmail.com>
2024-07-27 16:55:36 -0400

    Styled review component

--- appone/forms.py
+++ appone/forms.py
@@ -5,14 +5,23 @@ class PrepForm(forms.ModelForm):
     class Meta:
         model = Prep
         fields = ["subject"]
+        widgets = {
+            'subject': forms.TextInput(attrs={'class': 'form-control'})
+        }
 
 class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
         fields = ["subject"]
+        widgets = {
+            'subject': forms.TextInput(attrs={'class': 'form-control'})
+        }
 
 class EmailSignupForm(forms.ModelForm):
     class Meta:
         model = EmailSubscriber
         fields = ["email"]
+        widgets = {
+            'email': forms.EmailInput(attrs={'class': 'form-control'})
+        }
 
