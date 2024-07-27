3321c00e9c3b12ea43c116e5fd16cce8b3998cd3
MargaretLai <MargaretLUI01@gmail.com>
2024-07-27 11:43:02 -0400

    Used OpenAI to process user input

--- appone/views.py
+++ appone/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render, redirect, HttpResponse
 from .forms import PrepForm, ReviewForm
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+openai_key = os.environ.get("OPENAI_API_KEY")
+client = OpenAI(api_key = openai_key)
 
 # Create your views here.
 def home(request):
@@ -11,7 +18,21 @@ def review(request):
         if form.is_valid():
             saved_form = form.save()
             subject = saved_form.subject
-            print(subject)
+
+
+            # Process subject with OpenAI API
+            user_message = "Give me 5 key points on " + subject + ". Return the key points only."
+
+            completion = client.chat.completions.create(
+                model="gpt-4o-mini",
+                messages=[
+                    {"role": "system", "content": "You are an insturctor, skilled in helping students review different courses and topics for exams."},
+                    {"role": "user", "content": user_message}
+                ]
+            )
+
+            print(completion.choices[0].message)
+
             return redirect("review") # Return to the same page rn
     else:
         form = ReviewForm()
@@ -24,6 +45,21 @@ def prep(request):
         if form.is_valid():
             saved_form = form.save()
             subject = saved_form.subject
+
+            # Process subject with OpenAI API
+            user_message = "Give me 5 practice exam quetions on " + subject + ". Return questions and answers only."
+
+            completion = client.chat.completions.create(
+                model="gpt-4o-mini",
+                messages=[
+                    {"role": "system", "content": "You are an insturctor, skilled in helping students review different courses and topics for exams."},
+                    {"role": "user", "content": user_message}
+                ]
+            )
+
+            print(completion.choices[0].message)
+
+
             return redirect("prep") # Return to the same page rn
     else:
         form = PrepForm()
